98bbb5fec467df3d3c654bcae0552acf992cfbc8
windspeed326 <nikitagontar9@gmail.com>
2025-12-18 23:18:40 +0300

    Add files via upload

---
+++ Praktikum/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Praktikum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmBIz = new System.Windows.Forms.ComboBox();
+            this.cmBb = new System.Windows.Forms.ComboBox();
+            this.btnKonvert = new System.Windows.Forms.Button();
+            this.txtSumma = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(251, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Конвертер валют";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Из:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "В:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Сумма:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Результат:";
+            // 
+            // cmBIz
+            // 
+            this.cmBIz.FormattingEnabled = true;
+            this.cmBIz.Location = new System.Drawing.Point(143, 114);
+            this.cmBIz.Name = "cmBIz";
+            this.cmBIz.Size = new System.Drawing.Size(299, 21);
+            this.cmBIz.TabIndex = 5;
+            // 
+            // cmBb
+            // 
+            this.cmBb.FormattingEnabled = true;
+            this.cmBb.Location = new System.Drawing.Point(143, 150);
+            this.cmBb.Name = "cmBb";
+            this.cmBb.Size = new System.Drawing.Size(299, 21);
+            this.cmBb.TabIndex = 6;
+            // 
+            // btnKonvert
+            // 
+            this.btnKonvert.Location = new System.Drawing.Point(484, 133);
+            this.btnKonvert.Name = "btnKonvert";
+            this.btnKonvert.Size = new System.Drawing.Size(141, 33);
+            this.btnKonvert.TabIndex = 7;
+            this.btnKonvert.Text = "Конверт";
+            this.btnKonvert.UseVisualStyleBackColor = true;
+            this.btnKonvert.Click += new System.EventHandler(this.btnKonvert_Click);
+            // 
+            // txtSumma
+            // 
+            this.txtSumma.Location = new System.Drawing.Point(164, 185);
+            this.txtSumma.Name = "txtSumma";
+            this.txtSumma.Size = new System.Drawing.Size(278, 20);
+            this.txtSumma.TabIndex = 8;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(181, 224);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(261, 20);
+            this.txtResult.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(113, 263);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(511, 132);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Курсы валют к RUB";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "1 USD = 77.70 RUB";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 52);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "1 EUR = 90.34 RUB";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "1 CNY = 10.96 RUB";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 96);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(113, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "1 KRW = 0.0670 RUB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSumma);
+            this.Controls.Add(this.btnKonvert);
+            this.Controls.Add(this.cmBb);
+            this.Controls.Add(this.cmBIz);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmBIz;
+        private System.Windows.Forms.ComboBox cmBb;
+        private System.Windows.Forms.Button btnKonvert;
+        private System.Windows.Forms.TextBox txtSumma;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Praktikum/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktikum
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] currencies = { "RUB", "USD", "EUR", "CNY", "KRW" };
+
+        // Курсы относительно RUB
+        private readonly double[] rubRates = { 1, 77.70, 90.34, 10.96, 0.0670 };
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeComboBoxes();
+        }
+        
+        private void InitializeComboBoxes()
+        {
+            cmBIz.Items.AddRange(currencies);
+            cmBb.Items.AddRange(currencies);
+            cmBIz.SelectedIndex = 0;
+            cmBb.SelectedIndex = 0;
+        }
+
+        private void btnKonvert_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtSumma.Text, out double amount) && amount > 0)
+            {
+                int fromIndex = cmBIz.SelectedIndex;
+                int toIndex = cmBb.SelectedIndex;
+
+                // Конвертация через RUB как базовую валюту
+                double rubAmount = amount * rubRates[fromIndex];
+                double result = rubAmount / rubRates[toIndex];
+
+                txtResult.Text = $"{result:F2}";
+            }
+            else
+            {
+                MessageBox.Show("Введите корректную сумму");
+            }
+        }
+    }
+}
